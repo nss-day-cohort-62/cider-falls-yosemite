ceb73d5579f31a676f9243c9ea7c5e4aab20effd
Kristi Cornett <kristi@cornettclan.com>
2023-01-24 15:46:13 -0600

    trying

--- scripts/database.js
+++ scripts/database.js
@@ -125,16 +125,16 @@ const database = {
 }
 
 
-// export const getAreas = () => {
-//     return database.areas.map(area => ({...area}))
-// } 
+ export const getAreas = () => {
+    return database.areas.map(area => ({...area}))
+ } 
 
-// export const getActivities = () => {
-//     return database.activities.map(activity => ({...activity}))
-// }
-// export const getAreaActivities = () => {
-//     return database.areaActivities.map(areaActivity => ({...areaActivity}))
-// }
+ export const getActivities = () => {
+    return database.activities.map(activity => ({...activity}))
+ }
+ export const getAreaActivities = () => {
+    return database.areaActivities.map(areaActivity => ({...areaActivity}))
+ }
 
 export const getAreas = () => {
     return database.areas.map(area => ({...area}))

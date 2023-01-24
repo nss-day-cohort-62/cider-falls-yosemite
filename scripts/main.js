f900727d4554d3f1f90f373568f1783c1d618429
Mary Williams <sbyardnss@gmail.com>
2023-01-24 13:42:07 -0600

    database completed

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+import { areasHTML } from "./areas.js";
+
+const mainContainer = document.querySelector("#container")
+
+// const applicationHTML = 
+
+
+
+
+mainContainer.innerHTML = areasHTML()
+
